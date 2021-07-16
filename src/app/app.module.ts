ed3582278f7724a317a5bb3c0a9a96c06307d1c6
Fejiro Gospel <fejiro.gospel@gmail.com>
2021-07-16 13:15:29 +0100

    page layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { RecipientApi } from './services/recipient-api.service';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AppRoutingModule
   ],
   providers: [RecipientApi],
   bootstrap: [AppComponent]
