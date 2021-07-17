f1e7f12c3cef7092067b78f2df4bb51205b0c13e
Fejiro Gospel <fejiro.gospel@gmail.com>
2021-07-17 19:43:09 +0100

    selected recipients

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,21 @@ import { AppComponent } from './app.component';
 import { RecipientApi } from './services/recipient-api.service';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -13,9 +28,11 @@ import { AppRoutingModule } from './app-routing.module';
     HomeComponent
   ],
   imports: [
-    BrowserModule, AppRoutingModule
+    BrowserModule, AppRoutingModule, FormsModule, HttpClientModule,
+    BrowserAnimationsModule, NzIconModule, NzAutocompleteModule,
+    NzInputModule, NzCheckboxModule, NzButtonModule, NzCollapseModule
   ],
-  providers: [RecipientApi],
+  providers: [RecipientApi, { provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipientApi } from '../services/recipient-api.service';
+
+interface Company {
+  id: number;
+  name: string;
+}
+
+interface RecipientList {
+  name: string;
+  companyRecipients: number[];
+  emailRecipients: string[];
+}
+
+interface EmailRecipient {
+  domain: string,
+  recipients: string[]
+}
+
+interface CheckOptions {
+  label: string;
+  value: string;
+  disabled: boolean;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,10 +29,147 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  constructor(private recipientApi: RecipientApi) { }
 
-  constructor() { }
+  companies: Company[] = [];
+  selectedCompanies: string[] = []
+  selectedEmailRecipients: object[] = []
+  recipientList: object[] = [];
+  inputValue: string = '';
+  filteredOptions: string[] = [];
+  options: string[] = [];
+  checkOptions = []
 
-  ngOnInit(): void {
+  totalSelected(): number {
+    const selectedCompanies = this.selectedCompanies.length
+    const selectedEmails = this.selectedEmailRecipients.reduce((counter, email: EmailRecipient) => {
+      email.recipients.forEach(() => {
+        counter += 1
+      })
+      return counter
+    }, 0)
+    return selectedCompanies + selectedEmails
   }
 
+  onChange(value: string): void {
+    this.filteredOptions = this.options.filter(option => option.toLowerCase().indexOf(value.toLowerCase()) !== -1);
+  }
+
+  emailSubmissionHandler() {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    const validEmail = emailRegex.test(this.inputValue)
+    if (validEmail) {
+      const domain = `@${this.inputValue.split("@")[1]}`
+      const existingDomain = this.selectedEmailRecipients.find((r: EmailRecipient) => r.domain === domain)
+      if (existingDomain) {
+        this.selectedEmailRecipients = this.selectedEmailRecipients.map((r: EmailRecipient) => {
+          if (r.domain === domain && !r.recipients.includes(this.inputValue)) {
+            r.recipients.push(this.inputValue)
+          }
+          return r
+        })
+      } else {
+        this.selectedEmailRecipients = [...this.selectedEmailRecipients, {
+          domain,
+          active: false,
+          recipients: [this.inputValue]
+        }]
+      }
+      // find and remove from options
+      this.removeFromSearchOptions(this.inputValue)
+    }
+  }
+
+  companySubmissionHandler() {
+    if (this.options.includes(this.inputValue)) {
+      this.selectedCompanies = [...this.selectedCompanies, this.inputValue]
+      // find and remove from options
+      this.removeFromSearchOptions(this.inputValue)
+    }
+  }
+
+  onSubmit(): void {
+    if (this.inputValue.includes('@')) {
+       this.emailSubmissionHandler()
+    } else {
+       this.companySubmissionHandler()
+    }
+  }
+
+  addToSearchOptions(value: string): void {
+    // update search options
+    this.options = [...this.options, value].sort()
+  }
+
+  removeFromSearchOptions(value: string): void {
+    // update search options
+    this.options = this.options.filter((option) => option !== value)
+  }
+
+  removeCompany(value): void {
+    this.selectedCompanies = this.selectedCompanies.filter((option) => option !== value)
+    this.addToSearchOptions(value)
+  }
+
+  removeRecipientGroup({ domain, recipients }) {
+    recipients.forEach(recipient => {
+      this.removeRecipient(domain, recipient)
+    });
+  }
+
+  removeRecipient(domain, recipient) {
+    this.selectedEmailRecipients = this.selectedEmailRecipients.map((option: EmailRecipient) => {
+      if (option.domain === domain) {
+        option.recipients = option.recipients.filter((o) => o !== recipient)
+      }
+      return option
+    })
+    this.addToSearchOptions(recipient)
+  }
+
+  deduping(data): string[] {
+    // Ensuring no duplicates
+    return data.filter((c, index) => {
+      return data.indexOf(c) === index;
+    });
+  }
+
+  getRecipients() {
+    const recipients = []
+    this.recipientList.forEach((recipient: RecipientList) => {
+      recipient.emailRecipients.forEach((r: string) => recipients.push(r))
+    })
+    return recipients;
+  }
+
+  getCompanies() {
+    return this.companies.map((company) => company.name)
+  }
+
+  processingSearchOptions(): string[] {
+    const recipients = this.getRecipients()
+    const companies = this.getCompanies()
+    return this.deduping([...companies, ...recipients])
+  }
+
+  processingCheckOptions(): CheckOptions[] {
+    return this.recipientList.map((r: RecipientList) => {
+      return {
+        label: r.name,
+        value: r.name,
+        disabled: true
+      }
+    })
+  }
+
+  ngOnInit(): void {
+    this.recipientApi.getCompanies().subscribe(
+      (listCompanies) => this.companies = listCompanies
+    )
+    this.recipientApi.getRecipientLists().subscribe(
+      (listRecipients) => this.recipientList = listRecipients
+    )
+    this.options = this.processingSearchOptions()
+    this.checkOptions = this.processingCheckOptions()
+  }
 }
